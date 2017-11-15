67458916d2eb71001c8725b02ba407343e37c3f3
Sean Charoutanh <scharoutanh@gmail.com>
2017-11-14 21:31:05 -0600

    added checkBox

---
+++ CheckBox/EntryPoint.cs
@@ -0,0 +1,40 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+class EntryPoint
+{
+    static void Main()
+    {
+        string url = "http://testing.todvachev.com/special-elements/check-button-test-3/";
+        string option = "3";
+        string cssPath = "#post-33 > div > p:nth-child(8) > input[type=\"checkbox\"]:nth-child("+ option +")"; // supported by most browser & works faster than xpath
+
+        IWebDriver driver = new ChromeDriver();
+
+        driver.Navigate().GoToUrl(url);
+
+        IWebElement cssPathElement = driver.FindElement(By.CssSelector(cssPath));
+       
+        if (cssPathElement.GetAttribute("checked") == "true")
+            GreenMessage("The checkbox is checked!");
+        else
+            RedMessage("The checkbox is NOT checked!");
+
+        driver.Quit();
+    }
+
+    private static void RedMessage(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(message);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+
+    private static void GreenMessage(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(message);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+}
